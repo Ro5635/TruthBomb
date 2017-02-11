b769962674d2beae969cdfb9aff8f269e038adb9
Jenny Myers <jenny.myers@btinternet.com>
2017-02-11 15:06:53 +0000

    More HTML

---
+++ FrontEndWebSite/TruthBombPHP.php
@@ -0,0 +1,6 @@
+<?php
+//for each array print data-target="<id>" data-slide-to="x", increase x by one each time, define in function
+//bbc, reuters, economist images (for now) 
+//slide label, data src, description
+$contents = array(array());
+
