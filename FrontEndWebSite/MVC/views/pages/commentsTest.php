6046de2e05a6d11e39d1f9186683e2cc4b3dcd1c
RobertC <ro5635@gmail.com>
2017-02-11 14:30:49 +0000

    Mesagaing System Bug Fix

--- FrontEndWebSite/MVC/views/pages/commentsTest.php
+++ FrontEndWebSite/MVC/views/pages/commentsTest.php
@@ -1,6 +1,6 @@
 <?php
 
-echo '<home>';
+echo '<body>';
 
 
 
--- FrontEndWebSite/WebRoot/assets/JS/DiscusScript.js
+++ FrontEndWebSite/WebRoot/assets/JS/DiscusScript.js
@@ -11,7 +11,4 @@ s.src = '//www-truthbomb-online.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
-
 
