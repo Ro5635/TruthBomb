5c384996594fb976cc3336e88b3d4b0b5d303cd6
RobertC <ro5635@gmail.com>
2017-02-11 17:24:10 +0000

    Removal carasel, switch back to rows

--- FrontEndWebSite/MVC/views/pages/commentsTest.php
+++ FrontEndWebSite/MVC/views/pages/commentsTest.php
@@ -25,29 +25,17 @@ var_dump($images);
 echo '</pre>';
 
 
-echo '<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">';
-	echo '<div class="carousel-inner" role="listbox">';
 
 foreach ($images as $image) {
 
-	echo '<div class="carousel-item carousel-item-next carousel-item-left">';
 	
-		echo '<img src="' . $image['Imagesrc'] . '" class="img-fluid d-block" data-holder-rendered="true" ' . $image['alt'] . '">';
+		echo '<div class="row"><img src="' . $image['Imagesrc'] . '" class="img-fluid" ' . $image['alt'] . '"></div>';
 
-	echo '</div>';
+
 
 }
 
-echo '</div>';
 
-echo '<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">';
-echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
-echo '<span class="sr-only">Previous</span>';
-echo ' </a>';
-echo ' <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">';
-echo '<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span>';
-echo '</a></div>';
- 
  
     
   
