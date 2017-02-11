eef72782de11ab018dcd2f514fc13c07cb745cc9
RobertC <ro5635@gmail.com>
2017-02-11 17:03:02 +0000

    Addition of single bank of images and use of carasell

--- FrontEndWebSite/MVC/views/pages/commentsTest.php
+++ FrontEndWebSite/MVC/views/pages/commentsTest.php
@@ -6,9 +6,13 @@ echo '<body>';
 echo 'Testing Image Thing:';
 
 
-$bankID = ImageBank::attemptCreateNew("http://lorempixel.com/400/445/", "This is an image alt");
-ImageBank::attemptCreateAppendNew("http://lorempixel.com/300/445/", "Apple sauce", $bankID['BankID']);
-ImageBank::attemptCreateAppendNew("http://lorempixel.com/450/445/", "Merry Christmas", $bankID['BankID']);
+$bankID = 76903139;
+//$bankID['BankID']
+ImageBank::attemptCreateAppendNew("http://lorempixel.com/300/445/", "Apple sauce", $bankID );
+ImageBank::attemptCreateAppendNew("http://lorempixel.com/450/445/", "Merry Christmas", $bankID);
+ImageBank::attemptCreateAppendNew("http://lorempixel.com/450/445/", "Merry Bob", $bankID);
+ImageBank::attemptCreateAppendNew("http://lorempixel.com/500/445/", "Hi", $bankID);
+ImageBank::attemptCreateAppendNew("http://lorempixel.com/233/445/", "Mcdonalds", $bankID);
 
 
 $images = ImageBank::getBankID($bankID['BankID']);
@@ -19,11 +23,32 @@ var_dump($images);
 
 echo '</pre>';
 
+
+echo '<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">';
+	echo '<div class="carousel-inner" role="listbox">';
 foreach ($images as $image) {
+	echo '<div class="carousel-item carousel-item-next carousel-item-left">';
 	
-	echo '<div class="row"><img src="' . $image['Imagesrc'] . '"" class="img-fluid" alt="' . $image['alt'] . '"></div>';
+		echo '<img src="' . $image['Imagesrc'] . '"" class="img-fluid d-block" data-holder-rendered="true" ' . $image['alt'] . '">';
+	echo '</div>';
 
 }
+
+echo '</div>';
+
+echo '<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">';
+echo '<span class="carousel-control-prev-icon" aria-hidden="true"></span>';
+echo '<span class="sr-only">Previous</span>';
+echo ' </a>';
+echo ' <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">';
+echo '<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span>';
+echo '</a></div>';
+ 
+ 
+    
+  
+
+
 
 
 
