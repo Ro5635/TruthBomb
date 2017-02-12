95a4c9492f5bd21c74bd22f4d86131aca5e2e78b
RobertC <ro5635@gmail.com>
2017-02-12 11:54:27 +0000

    Form additions

--- FrontEndWebSite/MVC/views/pages/formSubmit.php
+++ FrontEndWebSite/MVC/views/pages/formSubmit.php
@@ -21,9 +21,49 @@
 	<input type="text" class="form-control" id="formName" placeholder="Enter name">
       </div>
       <div class="form-group">
-	<label for="formEmail">Email:</label>
-	<input type="text" class="form-control" id="formEmail" placeholder="Your email">
+	<label for="formEmail">Rating:</label>
+	<input type="text" class="form-control" id="formRating" placeholder="Your rating">
       </div>
+
+
+<div class="form-check">
+  <label class="form-check-label">
+    <input class="form-check-input" type="radio" name="ratingRadios"
+           id="ratingRadios1" value="1" checked>
+   1 Star
+  </label>
+</div>
+<div class="form-check">
+  <label class="form-check-label">
+    <input class="form-check-input" type="radio" name="ratingRadios"
+           id="ratingRadios2" value="2">
+     2 Star
+  </label>
+</div>
+<div class="form-check">
+  <label class="form-check-label">
+    <input class="form-check-input" type="radio" name="ratingRadios"
+           id="ratingRadios3" value="3">
+     3 Star
+  </label>
+</div>
+<div class="form-check">
+  <label class="form-check-label">
+    <input class="form-check-input" type="radio" name="ratingRadios"
+           id="ratingRadios4" value="4">
+     4 Star
+  </label>
+</div>
+<div class="form-check">
+  <label class="form-check-label">
+    <input class="form-check-input" type="radio" name="ratingRadios"
+           id="ratingRadios5" value="5">
+     5 Star
+  </label>
+</div>
+
+
+
       <div class="form-group">
 	<label for="formWebsite">Website Address:</label>
 	<input type="text" class="form-control" id="formWebsite" placeholder="Website adress">
@@ -36,10 +76,10 @@
   </form>
   <div class="row">
     <div class="col-sm-1">
-      <button type="button" class="btn btn-primary">Submit</button>
+      <button type="button"  id= "subReviewButt" class="btn btn-primary">Submit</button>
     </div>
     <div class="col-sm-1">
-      <a href="TruthBombSite.html">
+      <a href="/pages/home">
 	<button type="button" class="btn btn-danger">Cancel</button>
       </a>
     </div>
---
+++ FrontEndWebSite/WebRoot/assets/JS/formjs.js
@@ -0,0 +1,42 @@
+//Form JS
+
+$(document).ready(function(){
+
+
+	$('#subReviewButt').click(function(){
+
+		siteURL = $('#formWebsite').val():
+
+		userRating = $('#alignment').val();
+
+		var dataToTransmit = 'siteurl=' + siteURL + '&rating=' + userRating;
+
+		$.ajax({
+			url: "/ajax/registervote",
+			type: "POST",
+			data: dataToTransmit,
+			cache: false,
+			success: function(reternedData) {
+
+				var recivedJSON = JSON.parse(reternedData);
+
+				
+				window.location.href = recivedJSON.redirectTo;
+				alert('Current Rating for URL: ' + recivedJSON.CurrentRating );
+				window.location.replace("/pages/home");
+
+
+			}
+
+		});
+
+
+	});
+
+
+	$(".alignment .btn").click(function() {
+    // whenever a button is clicked, set the hidden helper
+    $("#alignment").val($(this).text());
+}); 
+
+});
