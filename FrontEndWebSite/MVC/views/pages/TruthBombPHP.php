a980fdb2417cf3e659f510dc99fd6a48f0ef9a8e
Lewis Blackburn <blackbul@aston.ac.uk>
2017-02-12 12:34:17 +0000

    Changed inner styling of elements in carousel

--- FrontEndWebSite/MVC/views/pages/TruthBombPHP.php
+++ FrontEndWebSite/MVC/views/pages/TruthBombPHP.php
@@ -16,7 +16,7 @@ foreach ($items as $item){
   else{
     echo '<div class="carousel-item">';
   }
-  echo '<img class="d-block img-fluid width="500px" height="345px" src="' . $item['Imagesrc'] . '" alt="' . $item['alt'] . '">';
+  echo '<img class="d-block img-fluid style="width:500px;height:345px;" src="' . $item['Imagesrc'] . '" alt="' . $item['alt'] . '">';
   echo '</div>';
   $count++;
 }
