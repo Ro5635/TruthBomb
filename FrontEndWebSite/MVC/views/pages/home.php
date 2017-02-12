3607aaca06488e8c2497aa2e9e83fd1a6da5e731
Lewis Blackburn <blackbul@aston.ac.uk>
2017-02-12 12:53:44 +0000

    Added filler column for page

--- FrontEndWebSite/MVC/views/pages/home.php
+++ FrontEndWebSite/MVC/views/pages/home.php
@@ -24,24 +24,27 @@ include($_SERVER['DOCUMENT_ROOT'].'/../PHPIncludes/Views/Menus/StdMenuBar.php');
 	<!--heading for trusted sources section -->
 	<h3>Truth Bomb's most trusted sources</h3>
       </div>
+    </div>
+<div class="row">
       <div class="col-sm-4">
 	<!-- where forum will be, fills up the rest of the pages columns -->
 	<a href="/pages/review">
 	  <button type="button" class="btn btn-primary">Submit a review</button>
 	</a>
       </div>
-    </div>
-    <div class="row">
       <!-- Where slideshow will be for trusted sources, created in a new row -->
         <div class="col-sm-8">
 	<!-- Carousel code starts here -->
   <?php require_once($_SERVER['DOCUMENT_ROOT'].'../MVC/views/pages/TruthBombPHP.php');?>
   <!-- Carousel code ends here -->
         </div>
+    </div>
+          <div class="row">
+            <div class="col-sm-8"></div>
         <div class="col-sm-4">
 	  </br>
       	  <a href="/pages/donate">
-	    <button type="button" class="btn btn-primary">Donate!!!</button>
+	        <button type="button" class="btn btn-primary">Donate!!!</button>
 	  </a>
 	</div>
      </div>
